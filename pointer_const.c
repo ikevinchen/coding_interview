f46a3787f9152d512cb70876447af1273f81a80a
HP chen <ikevinchen@me.com>
2018-09-14 21:10:59 +0800

    update pointer const case

--- pointer_const.c
+++ pointer_const.c
@@ -1,71 +1,138 @@
+
 #include <stdio.h>
 
-int arr_global[3] = {7, 8, 9};
-void func(const int **arr_pptr) {
-    *arr_pptr = arr_global;
+#if 0
+see the position between "const" and "*"
+
+const* : pointer to the const
+*const : const pointer
+
+    ex.
+    (const int) * ==> pointer       to "const int"
+    (int const) *
+    int (* const) ==> const pointer to "int"
+
+const** ==> (const *)* : pointer       to pointer to the const
+*const* ==> (*const)*  : pointer       to const pointer
+**const ==> *(*const)  : const pointer to the pointer
+
+從右邊解讀回來
+const*const*const
+   step1. (const*const)(*const) ==> const pointer to "const*const"
+   step2. const(*const) ==> const pointer to const
+==> const pointer to const pointer to const
+
+
+const*const*
+    step1. (const*const)*  ==> pointer to "const*const"
+    step2. const(*const) ==> const pointer to const
+==> pointer to const pointer to const
+
+    ex.
+    ((const int) *)* ==> pointer       to pointer to "const int"
+    ((int const) *)*
+    (int (* const))* ==> pointer       to const pointer to "int"
+    int *(* const)   ==> const pointer to pointer to "int"
+
+int var;
+&var          ==> pointer to "int"  
+
+const int c_var; 
+&c_var        ==> pointer to "cosnt int"
+#endif
+
+int global = 10;
+void func_a(const int* ptr) {  /* pointer to "const int" */
+    //*ptr = 10;       //compile error
+       ptr = &global;  //compile okay
 }
 
-int global_c = 5;
-int global_d = 6;
-/* cannot modify "**pptr" ==> "int" */
-void func_a(const int **pptr) {
-    *pptr = &global_c;
-    //**pptr = 10; /* compile error */
+void func_b(int *const ptr) { /* const pointer to "int" */
+      *ptr = 10;       //compile okay
+    // ptr = &global;  //compile error
 }
 
-/* cannot modify "*pptr" ==> "int *" */
-void func_b(int *const *pptr) {
-    //*pptr = &global_c; /* compile error */
-    **pptr = 10;
+void func_c(int* ptr)       { /*       pointer to "int" */
+      *ptr = 10;       //compile okay
+       ptr = &global;  //compile okay
 }
 
-//void func_3(int **const pptr) {
-//    /* cannot modify "pptr" */
-//    *pptr = &global_c;
-//}
+void func_d(const int **pptr) { /* pointer to pointer to "const int" */
+    //**pptr = 10;         //compile error
+       *pptr = &global;    //compile okay
+        pptr = NULL;       //compile okay
+}
+
+void func_e(int * const* pptr) { /* pointer to const pointer to int */
+      **pptr = 10;         //compile okay
+    // *pptr = &global;    //compile error
+        pptr = NULL;       //compile okay
+}
+
+void func_f(int **const pptr) { /* const pointer to pointer to int */
+      **pptr = 10;         //compile okay
+       *pptr = &global;    //compile okay
+    //  pptr = NULL;       //compile error
+}
 
 int main()
 {
-    /*
-        1. const int a  = int const a
-        2. const int *a = int const *a
-           int* const a
-     */
+    int local   = 30;
+    const int c_local = 40;
 
-    int local = 10;
-    int *ptr_1 = &local;
-    const int *ptr_2 = &local;
-    int* const ptr_3 = &local;
-    func_a(&ptr_1); /* compile warning :
-                       func_1 do not want to modify "int"
-                       however *ptr_1 could be modified here
+    /* pass the pointer to int */
+    func_a(&local);
+    func_b(&local);
+    func_c(&local);
+
+    /* pass the pointer to const int */
+    func_a(&c_local);
+    func_b(&c_local); /* compile warning */
+    func_c(&c_local); /* compile warning */
+
+    int *ptr_1        = &local;
+    int *ptr_1_c      = &c_local;  /* compile warning */
+    const int *ptr_2  = &local; 
+    const int *ptr_2_c= &c_local; 
+    int *const ptr_3  = &local; 
+    int *const ptr_3_c= &c_local;  /* compile warning */
+
+    /* pass the pointer to "pointer to int" */
+    func_d(&ptr_1); /* compile warning,
+                            func_d : pointer of XXX
+                            pass.  : pointer of YYY
+                            (XXX = pointer to const int,
+                             YYY = pointer to int
+                            not the same type)
+                       ref. http://c-faq.com/ansi/constmismatch.html
+                    */
+    func_e(&ptr_1); /* compeile okay,
+                            func_e : pointer of const XXX
+                            pass.  : pointer of XXX
+                            (XXX = pointer to int)
+                        so compatible, just like how line 84 works
                      */
-    func_a(&ptr_2); /* compile okay */
-    func_a(&ptr_3); /* compile warning */
+    func_f(&ptr_1); /* compeile okay
+                            func_f : const pointer of XXX
+                            pass.  :       pointer of XXX
+                            (XXX = pointer to int)
+                        ==> okay
+                     */
 
+    /* pass the pointer to "pointer to const int" */
+    func_d(&ptr_2);
+    func_e(&ptr_2);  /* compile warning */
+    func_f(&ptr_2);  /* compile warning */
 
-    func_b(&ptr_1); /* compile okay? */
-    func_b(&ptr_2); /* compile warning */
-    func_b(&ptr_3); /* compile okay */
-    //*ptr = 10; /* compile error */
-
-    int arr_a[3] = {1, 2, 3};
-    int arr_b[3] = {4, 5, 6};
-    const int *arr_ptr = NULL;
-    arr_ptr = arr_a;
-    arr_ptr = arr_b;
-    func(&arr_ptr);
-
-
-    //arr_ptr[0] = 12; /* compile error */
-
-    int **arr_pptr = NULL;
-    *arr_pptr = arr_a;
-
-    #if 0
-    //int **arr_pptr = &arr_a; /* initialization from incompatible pointer type */
-    int (*arr_pptr)[3] = &arr_a;
-    #endif
+    /* pass the pointer to "const pointer to int" */
+    func_d(&ptr_3); /* compile warning */
+    func_e(&ptr_3);
+    func_f(&ptr_3); /* compile warning
+                            func_f : const pointer of XXX
+                            pass.  :       pointer of const XXX
+                            (XXX = pointer to int)
+                            ==> warning
+                     */
 
     return 0;
 }
