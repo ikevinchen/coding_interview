8345868ef1b0fda3011cf54005ff163ba4458c36
HP chen <ikevinchen@me.com>
2018-11-09 12:04:46 +0800

    update heap

--- heap.c
+++ heap.c
@@ -1,127 +1,103 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-#define MAX (100000)
-
-typedef struct node {
+typedef struct node_s {
     int data;
+    struct node_s* left;
+    struct node_s* right;
     int left_rank;
     int right_rank;
-    struct node* left_node;
-    struct node* right_node;
-} Node;
+} Node_t;
 
-void swap(int *a, int *b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
-
-void insert(Node** root, int insert_data) {
-    if (*root == NULL) {
-        Node *new_node = malloc(sizeof(Node));
-        new_node->data = insert_data;
-        new_node->left_rank = new_node->right_rank = 0;
-        new_node->left_node = new_node->right_node = NULL;
-        *root = new_node;
+void insert(int insert_value, Node_t **node) {
+    if (*node == NULL) {
+        *node = malloc(sizeof(Node_t));
+        (*node)->data = insert_value;
+        (*node)->left = (*node)->right = NULL;
+        (*node)->left_rank = (*node)->right_rank = 0;
     } else {
-        Node *node = *root;
-        if (insert_data < node->data) {
-            swap(&insert_data, &(node->data));
-        }
-        if (node->left_rank < node->right_rank) {
-            node->left_rank++;
-            insert(&node->left_node, insert_data);
+        int next_insert_value;
+
+        if (insert_value < (*node)->data) {
+            next_insert_value = (*node)->data;
+            (*node)->data = insert_value;
         } else {
-            node->right_rank++;
-            insert(&node->right_node, insert_data);
+            next_insert_value = insert_value;
+        }
+
+        if ((*node)->left_rank < (*node)->right_rank) {
+            (*node)->left_rank++;
+            insert(next_insert_value, &((*node)->left));
+        } else {
+            (*node)->right_rank++;
+            insert(next_insert_value, &((*node)->right));
         }
     }
 }
 
-int pop(Node **root) {
+void pop_recusive(Node_t **node) {
+
+    if ((*node)->left == NULL && (*node)->right == NULL) {
+        free(*node);
+        *node = NULL;
+        return;
+    }
+
+    Node_t **next_pop_node;
+    int replace_value;
+
+    if ((*node)->left == NULL) {
+        next_pop_node = &((*node)->right);
+        replace_value = (*node)->right->data;
+        (*node)->right_rank--;
+    } else if ((*node)->right == NULL) {
+        next_pop_node = &((*node)->left);
+        replace_value = (*node)->left->data;
+        (*node)->left_rank--;
+    } else if ((*node)->left->data < (*node)->right->data) {
+        next_pop_node = &((*node)->left);
+        replace_value = (*node)->left->data;
+        (*node)->left_rank--;
+    } else {
+        next_pop_node = &((*node)->right);
+        replace_value = (*node)->right->data;
+        (*node)->right_rank--;
+    }
+
+    (*node)->data = replace_value;
+    pop_recusive(next_pop_node);
+}
+
+void pop(Node_t **root) {
+
     if (*root == NULL) {
-        return -1;
+        printf("empty\n");
+    } else {
+        printf("pop %d\n", (*root)->data);
+        pop_recusive(root);
     }
 
-    Node* node = *root;
-    int answer = node->data;
-    if (node->left_node == NULL && node->right_node == NULL) {
-        answer = node->data;
-        free(node);
-        *root = NULL;
-        return answer;
-    }
-
-    /* find the smaller child to swap */
-    node->data = MAX;
-    while(1) {
-        Node *swap_child = NULL;
-        int swap_dir; /* 0:left 1:right */
-
-        if (node->left_node == NULL) {
-            swap_dir = 1;
-        } else if (node->right_node == NULL) {
-            swap_dir = 0;
-        } else if (node->left_node->data < node->right_node->data) {
-            swap_dir = 0;
-        } else {
-            swap_dir = 1;
-        }
-
-        if (swap_dir == 0) {
-            swap_child = node->left_node;
-        } else {
-            swap_child = node->right_node;
-        }
-        swap(&node->data, &swap_child->data);
-
-        if (swap_child->left_node == NULL && swap_child->right_node == NULL) {
-            if (swap_dir == 0) {
-                node->left_rank = 0;
-                node->left_node = NULL;
-            } else {
-                node->right_rank = 0;
-                node->right_node = NULL;
-            }
-            free(swap_child);
-            break;
-        }
-
-        node = swap_child;
-    }
-    return answer;
 }
 
 int main() {
-    Node *root = NULL;
-    insert(&root, 4);
-    insert(&root, 9);
-    insert(&root, 8);
-    insert(&root, 5);
-    insert(&root, 10);
-    insert(&root, 2);
+    Node_t *heap_root= NULL;
 
-    int min;
-    while((min = pop(&root)) != -1) {
-        printf("%d ", min);
-    }
-    printf("\n");
+    insert(3,&heap_root);
+    insert(1,&heap_root);
+    insert(5,&heap_root);
+    insert(2,&heap_root);
+    insert(6,&heap_root);
+    insert(7,&heap_root);
+    insert(4,&heap_root);
+
+    pop(&heap_root);
+    pop(&heap_root);
+    pop(&heap_root);
+    pop(&heap_root);
+    pop(&heap_root);
+    pop(&heap_root);
+    pop(&heap_root);
+    pop(&heap_root);
+
+    return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
