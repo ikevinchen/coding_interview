d18b97207479aa1a2b86b80e40bd7565b38b4dd2
ikevinchen <43058577+ikevinchen@users.noreply.github.com>
2018-12-04 12:38:54 +0800

    Update leetcode_10.c

--- leetcode/leetcode_10.c
+++ leetcode/leetcode_10.c
@@ -3,61 +3,61 @@
 #include <string.h>
 
 void run(const char* s, int s_i, const char* p, int p_i, int **DP)
-{	
-	if (p[p_i] == '\0') {
-		if (s[s_i] == '\0') DP[s_i][p_i] = 1;
-		else				DP[s_i][p_i] = 0;
-	} else if (p[p_i + 1] != '*') {
-		if (p[p_i] == '.' || p[p_i] == s[s_i]) {
-			if (s[s_i] == '\0') {
-				DP[s_i][p_i] = 0;
-			} else {
-				DP[s_i][p_i] = DP[s_i + 1][p_i + 1];
-			}
-		} else {
-			DP[s_i][p_i] = 0;
-		}
-	} else {
-		if (p[p_i] == '.' || p[p_i] == s[s_i]) {
-			if (s[s_i] == '\0') {
-				DP[s_i][p_i] = DP[s_i][p_i + 2];
-			} else {
-				DP[s_i][p_i] = DP[s_i + 1][p_i] | DP[s_i][p_i + 2];
-			}
-		} else {
-			DP[s_i][p_i] = DP[s_i][p_i + 2];
-		}			
-	}
+{    
+    if (p[p_i] == '\0') {
+        if (s[s_i] == '\0') DP[s_i][p_i] = 1;
+        else                DP[s_i][p_i] = 0;
+    } else if (p[p_i + 1] != '*') {
+        if (p[p_i] == '.' || p[p_i] == s[s_i]) {
+            if (s[s_i] == '\0') {
+                DP[s_i][p_i] = 0;
+            } else {
+                DP[s_i][p_i] = DP[s_i + 1][p_i + 1];
+            }
+        } else {
+            DP[s_i][p_i] = 0;
+        }
+    } else {
+        if (p[p_i] == '.' || p[p_i] == s[s_i]) {
+            if (s[s_i] == '\0') {
+                DP[s_i][p_i] = DP[s_i][p_i + 2];
+            } else {
+                DP[s_i][p_i] = DP[s_i + 1][p_i] | DP[s_i][p_i + 2];
+            }
+        } else {
+            DP[s_i][p_i] = DP[s_i][p_i + 2];
+        }            
+    }
 }
 
 int check(char *s, char *p) {
-	int **DP = NULL;
-	int s_i, p_i;
-	int answer;
-	
-	/* allocate */
-	DP = malloc((strlen(s)+1) * sizeof(int*));
-	for (s_i = 0; s_i <= strlen(s); s_i++) {
-		DP[s_i] = malloc((strlen(p)+1) * sizeof(int));
-	}
-	
-	/* run DP */
-	for (p_i = strlen(p); p_i >= 0; p_i--) {
-		for (s_i = strlen(s); s_i >= 0; s_i--) {
-			run(s, s_i, p, p_i, DP);
-		}
-	}
-	answer = DP[0][0];
-	
-	/* free */
-	for (s_i = 0; s_i <= strlen(s); s_i++) {
-		free(DP[s_i]);
-		DP[s_i] = NULL;
-	}
-	free(DP);
-	DP = NULL;
-	
-	return answer;
+    int **DP = NULL;
+    int s_i, p_i;
+    int answer;
+    
+    /* allocate */
+    DP = malloc((strlen(s)+1) * sizeof(int*));
+    for (s_i = 0; s_i <= strlen(s); s_i++) {
+        DP[s_i] = malloc((strlen(p)+1) * sizeof(int));
+    }
+    
+    /* run DP */
+    for (p_i = strlen(p); p_i >= 0; p_i--) {
+        for (s_i = strlen(s); s_i >= 0; s_i--) {
+            run(s, s_i, p, p_i, DP);
+        }
+    }
+    answer = DP[0][0];
+    
+    /* free */
+    for (s_i = 0; s_i <= strlen(s); s_i++) {
+        free(DP[s_i]);
+        DP[s_i] = NULL;
+    }
+    free(DP);
+    DP = NULL;
+    
+    return answer;
 }
 
 int main(){
